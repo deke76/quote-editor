88613f1bd279c3d4707965e5dd6149104f11283a
Darcy <darcy.lauder@shaw.ca>
2023-06-26 15:34:29 -0600

    Add LineItemsDate functionality

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,8 @@ class Quote < ApplicationRecord
   belongs_to :company
   validates :name, presence: true
 
+  has_many :line_item_dates, dependent: :destroy
+
   scope :ordered, -> { order(id: :desc) }
 
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
